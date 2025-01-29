6b39d780e15d56fecf006ca6fdb2fee844ba4fc5
AndreaBoiko <andriy.boyko35@gmail.com>
2025-01-29 02:08:34 +0100

    delete console

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,14 +1,9 @@
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
-export const CommentList = ({ comments }) => {
-  console.log(comments);
-  return (
-    <>
-      <div className="CommentList">
-        {comments.map(comment => (
-          <CommentInfo key={comment.id} comment={comment} />
-        ))}
-      </div>
-    </>
-  );
-};
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
+);
